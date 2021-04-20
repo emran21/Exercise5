890ceeb67a9505f027b479127c02974c794b491e
emran21 <61706353+emran21@users.noreply.github.com>
2021-04-21 04:45:54 +0800

    Add files via upload

---
+++ list.blade.php
@@ -0,0 +1,67 @@
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    }
+
+    th{
+        background-color: #dddddd;
+    }
+
+    td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    border: 1px solid #ddd;
+    padding: 8px;
+    }
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md12">
+        <table>
+<tr>
+<th>Title</th>
+<th>Author</th>
+<th>ISBN</th>
+<th>Category</th>
+<th>Page Count</th>
+</tr>
+
+<tr>
+<td>Anna Karenina</td>
+<td>Leo Tolstoy</td>
+<td>3080535908812</td>
+<td>Novel</td>
+<td>259</td>
+</tr>
+
+<tr>
+<td>To Kill a Mockingbird</td>
+<td>Harper Lee</td>
+<td>3657881291163</td>
+<td>Novel</td>
+<td>562</td>
+</tr>
+
+<tr>
+<td>One Hundred Years of Solitude</td>
+<td>Gabriel García Márquez</td>
+<td>8254789116123</td>
+<td>History</td>
+<td>356</td>
+</tr>
+
+<tr>
+<td>A Passage to India</td>
+<td>E.M. Forster  </td>
+<td>5689632306567</td>
+<td>Mystry</td>
+<td>304</td>
+</tr>
+
+</table>
+        </div>
+    </div>
+</div>
